74ad83835f5eaeaa94adab5adb20e4f9f1b2bcbd
Ian <ian.dunkelberg@outlook.com>
2023-12-13 13:13:02 -0800

    newthread.php is unused and unneeded

--- newthread.php
+++
@@ -1,6 +0,0 @@
-<?php 
-        //$sql = "INSERT INTO webtexting_threads (domain_uuid, remote_number, local_number, last_message) VALUES (:domain_uuid, :from, :to, NOW())";
-
-$url = $_SERVER["HTTP_ORIGIN"]."/app/webtexting/thread.php?extension_uuid=".$_GET["extension_uuid"]."&number=".$_GET["number"];
-header("Location: ".$url); 
-?>
