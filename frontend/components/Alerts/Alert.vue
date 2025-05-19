73c7f3fbaef2d18d7632176b1c0686bfb0118ba3
Ian <ian.dunkelberg@outlook.com>
2025-05-19 16:53:13 -0700

    still working on alerts. need to change styles depending on status#

---
+++ frontend/components/Alerts/Alert.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { emitter } from '../../lib/global';
+export type AlertData= {
+        error: String,
+        message: String,
+        status: Number,
+        type: String,
+}
+export default {
+    name: 'WTAlert',
+    props:{
+        error: String,
+        message: String,
+        status: Number,
+        type: String,
+    },
+    emits: {
+        'Close-Alert': String
+    },
+}
+//TODO Status switch to change class depending on status code (warning vs danger vs success)
+</script>
+<template>
+    <div class="alert alert-warning alert-dismissible fade show z-3" role="alert">
+        {{"Error Code: " + this.status + "\n" + this.message }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</template>
---
+++ frontend/components/Alerts/AlertFactory.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+import { emitter } from '../../lib/global';
+import Alert, { AlertData } from './Alert.vue';
+
+export default {
+  components: { Alert },
+    name: 'AlertFactory',
+    props:{
+    },
+    data() {
+        let AlertList:Array<AlertData>=[];
+        return {
+            AlertList
+        }
+    },
+    mounted() {
+        emitter.on('delete-template-failed', (payload:AlertData)=>{
+            console.log("failed to delete template make an alert about it.");
+            payload.message = "failed to delete template make an alert about it.";
+            payload.type='Error';
+                        this.AddAlert(payload);
+
+        })
+        emitter.on('delete-template-success', (payload)=>{
+            console.log("deleted template Sucessfully.");
+                        this.AddAlert(payload);
+
+        })
+        emitter.on('group-rename-failed', (payload)=>{
+            console.log("failed to rename group make an alert about it");
+                        this.AddAlert(payload);
+
+        })
+        emitter.on('group-rename-success', (payload)=>{
+            console.log("Succesfully renamed group.");
+                        this.AddAlert(payload);
+
+        })
+        emitter.on('template-save-complete', (payload)=>{
+            this.AddAlert(payload);
+        })
+    },
+    methods: {
+        AddAlert(arg:AlertData){
+            //create a new <Alert> component
+            //add it to the alert-box div
+                        console.log('alert box outbound message event listener');
+                        console.log(arg);
+                        this.$data.AlertList.push(arg);
+
+        }
+    },
+}
+</script>
+<template>
+    <div class="alert-box">
+        <Alert v-for="(alert,index) in this.$data.AlertList" :status="alert.status" :error="alert.error" :message="alert.message" :type="alert.type"/>
+    </div>
+</template>
+<style scoped>
+
+</style>
